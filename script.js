d753337deb8ab17870db4fd853a3300eb649a33c
yedoka <132571700+yedoka@users.noreply.github.com>
2024-01-30 22:26:13 +0100

    Update script.js

--- script.js
+++ script.js
@@ -4,3 +4,8 @@ function toggleMenu() {
   menu.classList.toggle("active");
   icon.classList.toggle("active");
 }
+
+function darkMode() {
+  let element = document.body;
+  element.classList.toggle("dark-mode");
+}
